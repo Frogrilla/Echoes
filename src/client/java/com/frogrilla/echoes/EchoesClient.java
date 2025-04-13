ef177dabf0db44822adfef260cb35ecf422f1f1d
Frogrilla <frogrillamodding@gmail.com>
2025-04-14 00:45:11 +0100

    ct table rendering crystal
    Testing out block entity rendering.
    Yet to implement the table's features.

--- src/client/java/com/frogrilla/echoes/EchoesClient.java
+++ src/client/java/com/frogrilla/echoes/EchoesClient.java
@@ -1,23 +1,29 @@
 package com.frogrilla.echoes;
 
+import com.frogrilla.echoes.common.init.EchoesBlockEntities;
 import com.frogrilla.echoes.common.init.EchoesBlocks;
 import com.frogrilla.echoes.common.init.EchoesParticles;
 import com.frogrilla.echoes.particle.EchoChargeParticle;
+import com.frogrilla.echoes.render.blockentity.CrystalisationTableRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.particle.SculkChargePopParticle;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class EchoesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ParticleFactoryRegistry.getInstance().register(EchoesParticles.ECHO_CHARGE, EchoChargeParticle.Factory::new);
 		ParticleFactoryRegistry.getInstance().register(EchoesParticles.CORRUPTED_ECHO_CHARGE, EchoChargeParticle.Factory::new);
+
 		BlockRenderLayerMap.INSTANCE.putBlock(EchoesBlocks.REFLECTOR, RenderLayer.getTranslucent());
 		BlockRenderLayerMap.INSTANCE.putBlock(EchoesBlocks.ECHO_ROD, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(EchoesBlocks.CORRUPTED_ECHO_ROD, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(EchoesBlocks.ECHO_LOTUS, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(EchoesBlocks.CRYSTALISATION_TABLE, RenderLayer.getCutout());
+
+		BlockEntityRendererFactories.register(EchoesBlockEntities.CRYSTALISATION_TABLE_BLOCK_ENTITY, CrystalisationTableRenderer::new);
 	}
 }
---
+++ src/client/java/com/frogrilla/echoes/render/blockentity/CrystalisationTableRenderer.java
@@ -0,0 +1,34 @@
+package com.frogrilla.echoes.render.blockentity;
+
+import com.frogrilla.echoes.common.block.entity.CrystalisationTableBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ModelTransformationMode;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.Vec3d;
+import org.joml.AxisAngle4d;
+import org.joml.Vector3d;
+
+public class CrystalisationTableRenderer implements BlockEntityRenderer<CrystalisationTableBlockEntity> {
+    private static final ItemStack stack = new ItemStack(Items.ECHO_SHARD);
+
+    public CrystalisationTableRenderer(BlockEntityRendererFactory.Context ctx) {}
+    @Override
+    public void render(CrystalisationTableBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        matrices.push();
+
+        matrices.translate(0.5, 0.825, 0.5);
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((entity.getWorld().getTime() + tickDelta) * 5));
+        matrices.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(90));
+        matrices.translate(0, -0.125f, 0);
+
+        MinecraftClient.getInstance().getItemRenderer().renderItem(stack, ModelTransformationMode.GROUND, light, overlay, matrices, vertexConsumers, entity.getWorld(), 0);
+        matrices.pop();
+    }
+}
